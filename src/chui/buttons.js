4905a4caa351c48bef0b2bce1aafc2f000106b5d
Robert Biggs <robert.biggs@sourcebits.com>
2013-12-30 19:18:59 -0800

    Removed unnecessary parts of button.js

--- src/chui/buttons.js
+++ src/chui/buttons.js
@@ -7,18 +7,11 @@
     ///////////////////////////////////
     $('body').on('singletap', '.button', function() {
       var $this = $(this);
-      if ($this.parent('.tabbar')[0]) return;
       if ($this.parent('.segmented')[0]) return;
       $this.addClass('selected');
       setTimeout(function() {
         $this.removeClass('selected');
       }, 500);
-      if (this.classList.contains('show-popover')) {
-        $this.addClass('selected');
-        setTimeout(function() {
-          $this.removeClass('selected');
-        },500);
-      }
     });
   });
 })(window.jQuery);
